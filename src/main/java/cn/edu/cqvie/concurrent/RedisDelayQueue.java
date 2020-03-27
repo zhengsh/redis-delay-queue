cfc97a0af28af0b3db58cc4d38736d2ca02a0ed4
zhengshaohong <zhengshaohong123>
2020-03-27 20:23:46 +0800

    initial commit

---
+++ src/main/java/cn/edu/cqvie/concurrent/RedisDelayQueue.java
@@ -0,0 +1,8 @@
+package cn.edu.cqvie.concurrent;
+
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+
+public class RedisDelayQueue<E extends Delayed> extends DelayQueue<E> {
+
+}
